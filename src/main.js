b36f9fc440dbac42a630430d2bf7aa239c4ad666
nikonew <121386211+nikonew@users.noreply.github.com>
2023-12-13 08:15:56 +0000

    two commit

--- src/main.js
+++ src/main.js
@@ -1,25 +1,14 @@
 // Импорт вендоров и утилит, не удаляйте его
 import "./vendor";
-import CatalogueApiService from './api-service/catalogue-api-service.js';
-import CatalogueModel from './model/catalogue-model.js';
 import { ImageSlider } from "./utils/image-slider";
 import { iosVhFix } from "./utils/ios-vh-fix";
 import { modals, initModals } from "./modals/init-modals";
 
-const AUTHORIZATION = 'Basic hS2sfS44wcl1sa2j';
-const END_POINT = 'https://grading.objects.pages.academy';
-
-const catalogueModel = new CatalogueModel(
-  {catalogueApiService: new CatalogueApiService(END_POINT, AUTHORIZATION)})
-
-const wrapperElement = document.querySelector('.wrapper');
-const mainElement = wrapperElement.querySelector('main')
-const presenter = new Presenter({
-  container: mainElement, catalogueModel
-});
 
 // Ваши импорты...
 import Presenter from './presenter.js';
+import CatalogueApiService from './api-service/catalogue-api-service.js';
+import CatalogueModel from './model/catalogue-model.js';
 
 
 // Код для работы попапов, не удаляйте его
@@ -48,9 +37,23 @@ window.addEventListener("DOMContentLoaded", () => {
   // ------------
 
   // Ваш код...
+  const AUTHORIZATION = 'Basic hS2sfS44wcl1sa2j';
+  const END_POINT = 'https://grading.objects.pages.academy';
+  
+  const catalogueModel = new CatalogueModel(
+    {catalogueApiService: new CatalogueApiService(END_POINT, AUTHORIZATION)})
+  
+  const wrapperElement = document.querySelector('.wrapper');
+  const mainElement = wrapperElement.querySelector('main')
+  const presenter = new Presenter({
+    container: mainElement, catalogueModel
+
+  });
+
+  presenter.init()
 
 });
 
 
-presenter.init()
-catalogueModel.init()
+
+
